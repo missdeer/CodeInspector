4b7e7c2dac837f44585552adcc2851dfd38e8567
missdeer <me@minidump.info>
2017-09-11 09:22:09 +0800

    (*)high dpi supported

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
+#include <QCoreApplication>
 #include "mainwindow.h"
 #include "settings.h"
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+
+    QCoreApplication::setApplicationName("Code Inspector");
+    QCoreApplication::setApplicationVersion("1.0");
+
     QApplication a(argc, argv);
 
     g_settings = new Settings;
