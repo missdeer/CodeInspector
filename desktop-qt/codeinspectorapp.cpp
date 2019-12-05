e03a1e3a5ee21cfa2070339300647d75ecc3f36f
Fan Yang <me@minidump.info>
2019-12-05 17:25:32 +0800

    (*)pre connect

--- desktop-qt/codeinspectorapp.cpp
+++ desktop-qt/codeinspectorapp.cpp
@@ -7,6 +7,7 @@
 
 CodeInspectorApp::CodeInspectorApp(QObject *parent) : QObject(parent), m_backend(new GodboltAgent(m_nam, this))
 {
+    m_nam.connectToHostEncrypted("ci.minidump.info");
     m_backend->initialize(this, g_settings->apiBaseURL());
 }
 
