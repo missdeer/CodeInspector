372b3fed8913e1edfcf6a1102f133518d365dad6
Fan Yang <fanyang@cadg.cn>
2021-06-23 14:07:48 +0800

    load plugins

--- desktop-qt/main.cpp
+++ desktop-qt/main.cpp
@@ -1,11 +1,37 @@
 #include <QApplication>
 #include <QMessageBox>
 #include <QPixmap>
+#include <QPluginLoader>
 #include <QSplashScreen>
 
 #include "codeinspectorapp.h"
 #include "mainwindow.h"
 #include "settings.h"
+
+// option plugins
+Q_IMPORT_PLUGIN(AstPlugin)
+Q_IMPORT_PLUGIN(CfgPlugin)
+Q_IMPORT_PLUGIN(GCCTreeRTLPlugin)
+Q_IMPORT_PLUGIN(LLVMIRPlugin)
+Q_IMPORT_PLUGIN(OptimizationPlugin)
+
+// runner plugins
+Q_IMPORT_PLUGIN(RextesterPlugin)
+Q_IMPORT_PLUGIN(WandboxPlugin)
+
+// tool plugins
+Q_IMPORT_PLUGIN(ClangFormatPlugin)
+Q_IMPORT_PLUGIN(ClangQueryPlugin)
+Q_IMPORT_PLUGIN(ClangTidyPlugin)
+Q_IMPORT_PLUGIN(IncludeWhatYouUsePlugin)
+Q_IMPORT_PLUGIN(LDDPlugin)
+Q_IMPORT_PLUGIN(LLVMMCAPlugin)
+Q_IMPORT_PLUGIN(OSACAPlugin)
+Q_IMPORT_PLUGIN(PaholePlugin)
+Q_IMPORT_PLUGIN(PSVStudioPlugin)
+Q_IMPORT_PLUGIN(ReadELFPlugin)
+Q_IMPORT_PLUGIN(StringsPlugin)
+Q_IMPORT_PLUGIN(X86To6502Plugin)
 
 int main(int argc, char *argv[])
 {
@@ -39,6 +65,10 @@ int main(int argc, char *argv[])
         QMessageBox::critical(nullptr, QObject::tr("Critical error"), QObject::tr("SSL not supported, exit now."), QMessageBox::Ok);
         return 1;
     }
+
+    splash.showMessage(QObject::tr("Loading plugins..."));
+    const auto staticInstances = QPluginLoader::staticInstances();
+    qDebug() << "there are " << staticInstances.length() << " static plugins";
 
     splash.showMessage(QObject::tr("Creating application instance..."));
     CodeInspectorApp app;
